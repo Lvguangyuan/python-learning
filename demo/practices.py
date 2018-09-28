4c8b1695c7792636985bf39c31182afbe4bb8aba
tyrion.lv <tyrion.lv@perficient.com>
2018-09-28 16:42:53 +0800

    learning generator.

--- demo/practices.py
+++ demo/practices.py
@@ -1,5 +1,3 @@
-
-
 def find_min_max(data):
     min_value = max_value = data[0]
     for i, value in enumerate(data):
@@ -8,4 +6,14 @@ def find_min_max(data):
         elif value < min_value:
             min_value = value
     return max_value, min_value
+
+
+def fibonacci(max_times):
+    n, a, b = 0, 0, 1
+    while n < max_times:
+        yield b
+        a, b = b, a + b
+        n = n + 1
+    return "Done"
+
 
